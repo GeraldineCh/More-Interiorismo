6dab496b7f643ead63befa3959dc5c4db7151af6
mishmore <michellemore28@gmail.com>
2017-04-25 00:04:56 -0500

    restart

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -4,12 +4,24 @@ window.addEventListener("scroll", function(){
    if (currentScroll > lastScrollTop){
      document.getElementById("about").classList.remove("opacity");
    }
-   if (currentScroll > 310) {
+   if (currentScroll > 900) {
      document.getElementById("gallery").classList.remove("opacity");
    }
-   if (currentScroll > 910) {
+   if (currentScroll > 1500) {
      document.getElementById("contact").classList.remove("opacity");
    }
    lastScrollTop = currentScroll;
    console.log(window.pageYOffset);
 }, false);
+
+var vid = document.getElementById("video");
+if (vid.currentTime = 5) {
+  vid.currentTime = 0;
+  vid.play();
+}
+/*
+video.pause();
+video.currentTime = 0;
+video.load();
+*/
+console.log(vid.currentTime);
