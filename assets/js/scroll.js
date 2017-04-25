8ed1dac04c5c81e05089fc0b13384f8398f0a199
mishmore <michellemore28@gmail.com>
2017-04-24 23:08:56 -0500

    scroll divs opacity ok

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -4,5 +4,12 @@ window.addEventListener("scroll", function(){
    if (currentScroll > lastScrollTop){
      document.getElementById("about").classList.remove("opacity");
    }
+   if (currentScroll > 310) {
+     document.getElementById("gallery").classList.remove("opacity");
+   }
+   if (currentScroll > 910) {
+     document.getElementById("contact").classList.remove("opacity");
+   }
    lastScrollTop = currentScroll;
+   console.log(window.pageYOffset);
 }, false);
