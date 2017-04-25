0c0e5312f7c94289c909a5e94812bac3497bdf5d
Mishmore <michellemore28@gmail.com>
2017-04-25 17:12:29 -0500

    class active ok

--- assets/js/drag.js
+++ assets/js/drag.js
@@ -1,29 +1,30 @@
 var draggableDiv = document.getElementsByClassName("draggable-div");
-function removeAllClass() {
+function removeAllClass(className) {
   for (var i = 0; i < draggableDiv.length; i++) {
-    draggableDiv[i].classList.remove("active");
+    className[i].classList.remove("active");
   }
 }
-//Drag & Drop
+//Start to drag a picture
 document.addEventListener("dragstart", function(ev){
     ev.dataTransfer.setData("text", ev.target.id);
     var data = ev.dataTransfer.getData("text");
 }, false);
-
+//Dragging and adding acive class to show picture
 document.addEventListener("drag", function(ev) {
   var data = ev.target.id;
   console.log(data);
-  var x = document.getElementById(data);
-  x.classList.add("active");
+  var draggedDiv = document.getElementById(data);
+  draggedDiv.setAttribute("class","active");
 });
-
+//Remove visibility of shown picure
 document.addEventListener("drop", function(ev){
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
   ev.target.appendChild(document.getElementById(data));
-  removeAllClass()
+  var draggedDiv = document.getElementById(data);
+  draggedDiv.setAttribute("class", "draggable-div");
 }, false);
-
+//Drop picture into div
 var drop = document.getElementById("drop-div");
 drop.addEventListener("dragover", function(event) {
   event.preventDefault();
