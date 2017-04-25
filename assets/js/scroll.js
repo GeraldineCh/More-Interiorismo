dd499d0ab4f9320f924cc5b6dbbf48894a4d796a
mishmore <michellemore28@gmail.com>
2017-04-25 07:47:24 -0500

    logo hover

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -4,11 +4,19 @@ window.addEventListener("scroll", function(){
    if (currentScroll > lastScrollTop){
      document.getElementById("about").classList.remove("opacity");
    }
-   if (currentScroll > 900) {
+   if (currentScroll < 1000){
+     document.getElementsByClassName("logo")[0].classList.remove("black");
+     document.getElementsByClassName("logo")[0].classList.add("white");
+   }
+   if (currentScroll > 1000) {
+     document.getElementsByClassName("logo")[0].classList.remove("white");
+     document.getElementsByClassName("logo")[0].classList.add("black");
      document.getElementById("gallery").classList.remove("opacity");
    }
-   if (currentScroll > 1500) {
+   if (currentScroll > 1700) {
      document.getElementById("contact").classList.remove("opacity");
+     document.getElementsByClassName("logo")[0].classList.remove("black");
+     document.getElementsByClassName("logo")[0].classList.add("white");
    }
    lastScrollTop = currentScroll;
    console.log(window.pageYOffset);
