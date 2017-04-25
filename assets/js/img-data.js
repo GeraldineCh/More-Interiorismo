a87107a8e7b90def1619de586950dfee3e7c0bfb
Mishmore <michellemore28@gmail.com>
2017-04-25 14:24:54 -0500

    cursor grab

--- assets/js/img-data.js
+++ assets/js/img-data.js
@@ -6,7 +6,7 @@ var images = [{src:"assets/img/renders/deco01.jpg"},
               {src:"assets/img/renders/deco05.jpg"},
               {src:"assets/img/renders/deco06.jpg"},
               {src:"assets/img/renders/deco07.jpg"},
-              {src:"assets/img/renders/deco07.jpg"}
+              {src:"assets/img/renders/deco08.jpg"}
             ];
 
 var draggableDiv = document.getElementsByClassName("draggable-div");
@@ -15,8 +15,16 @@ for (var i = 0; i < draggableDiv.length; i++) {
   console.log(parent);
   parent.setAttribute("src", images[i].src);
 }
-
 /*
-var x = document.getElementById("img1");
-x.setAttribute("src", "assets/img/renders/deco01.jpg");
+var mosaico = document.getElementsByClassName("mosaico");
+for (var i = 0; i < (draggableDiv.length/4)+1; i++) {
+
+  var parentTop = document.createElement("div");
+  parentTop.classList.add("parent-top");
+  mosaico.appendChild(parentTop)
+  var img
+  var parent = draggableDiv[i];
+  console.log(parent);
+  parent.setAttribute("src", images[i].src);
+}
 */
