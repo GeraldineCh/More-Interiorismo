7ff17a89256db3f032dfcdfd601e3108ea63268b
nakarid <hamdal@gmail.com>
2017-04-25 11:48:51 -0500

    contacto funcional

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,54 +1,57 @@
-var nombre = document.getElementById("nombre");
-var mensaje= document.getElementById("mensaje");
-var soloLetras = function(elemento){
-      var codigoTecla = elemento.keyCode;
-      //console.log(codigoTecla);
-      if((codigoTecla >=97 && codigoTecla <= 122) || (codigoTecla >= 65 && codigoTecla <=90) || codigoTecla == 164){
-        return true;
-      } else {
+
+      var nombre = document.getElementById("name");
+      var email = document.getElementById("input-email");
+
+      var soloLetras=function(elemento){
+            var letraAscii = elemento.keyCode;
+            if((letraAscii >= 97 && letraAscii <= 122) || (letraAscii >= 65 && letraAscii <= 90) || letraAscii == 164){
+                return true;
+            } else {
+                this.nextElementSibling.setAttribute("style", "display:block");
+                this.nextElementSibling.innerText="* Must be only letters.";
+            }
+      }
+
+      nombre.onkeypress=soloLetras;
+      email.onkeypress=soloLetras;
+
+      /*var correovalido = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      if (!correovalido.test(email)){
         this.nextElementSibling.setAttribute("style", "display:block");
-        this.nextElementSibling.innerText="* Debe colocar solo letras.";
+        this.nextElementSibling.innerText="* Debe colocar un correo válido.";
+      } else {
+        return true;
       }
-    }
-nombre.onkeypress=soloLetras;
-mensaje.onkeypress=soloLetras;
 
-var inputs = document.getElementsByClassName("input-registro");
-
-    //el inputs[0] es porque solo sale el mensaje de ue llene ese campo en la primera opcion
-var validacionInput= function(elemento){
-        if (this.value.trim().length==0){
-        this.value="";
-        this.nextElementSibling.nextElementSibling.innerText="*Este campo es obligatorio";
+      var passvalido = function (elemento) {
+        if (pass.value > 6 && pass.value < 30){
+          return true;
+        } else {
+          this.nextElementSibling.setAttribute("style", "display:block");
+          this.nextElementSibling.innerText="* Su correo debe tener más de 6 caracteres.";
+        }
       }
-     else {
+
+      email.onkeypress=correovalido;
+      pass.onkeypress=passvalido;
+      */
+
+      var datos = document.getElementsByClassName("form-control");
+
+      var validateForm = function(elemento){
+          if (this.value.trim().length==0){
+            this.value="";
+            this.nextElementSibling.setAttribute("style", "display:block");
+            this.nextElementSibling.innerText="* Obligatory field";
+          } else{
             this.nextElementSibling.nextElementSibling.innerText="";
+          }
+          var datoCorrecto=this.value.charAt(0).toUpperCase()+this.value.substring(1).toLowerCase();
+          this.value=datoCorrecto;
       }
-      var datoCorrecto = this.value.charAt(0).toUpperCase()+this.value.substring(1).toLowerCase();
-      this.value=datoCorrecto;
-      //console.log(this.value);
-  }
-  for (var i=0; i<inputs.length; i++){
-  inputs[i].onblur=validacionInput;
-  }
 
-//var mensaje = document.getElementById("mensaje");
+      for (var i=0; i<datos.length; i++){
+      datos[i].onblur=validateForm;
+      }
 
-
-var boton = document.getElementById("registro");
-
-boton.addEventListener("click",function(){
-  var nombre = document.getElementById("nombre").value;
-  var email= document.getElementById("email").value;
-  var mensaje = document.getElementById("mensaje").value;
-  function newsletter(nombre, email, mensaje){
-    this.nombre = nombre;
-    this.email = email;
-    this.mensaje = mensaje;
-  }
-  var pclientes =[];
-  var cliente = new newsletter(nombre, email, mensaje);
-  //console.log(new newsletter(nombre, email, mensaje));
-  localStorage.setItem("nuevoPaciente",JSON.stringify(new newsletter(nombre, email, mensaje)));
-  document.getElementById("formula").reset();
-});
+      var boton = document.getElementById("login");
