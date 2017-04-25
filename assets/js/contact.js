2c6897e246d938f14c6a13a3f0445f75389aa26c
nakarid <hamdal@gmail.com>
2017-04-25 18:40:07 -0500

    validando email

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -4,12 +4,9 @@
 
       var soloLetras=function(elemento){
             var letraAscii = elemento.keyCode;
-            if((letraAscii >= 97 && letraAscii <= 122) || (letraAscii >= 65 && letraAscii <= 90) || letraAscii == 164){
+            if((letraAscii >= 97 && letraAscii <= 122) || (letraAscii >= 65 && letraAscii <= 90) || (letraAscii <= 164 && letraAscii <=165) || letraAscii == 32){
                 return true;
-<<<<<<< HEAD
-=======
                 this.nextElementSibling.setAttribute("style", "display:none");
->>>>>>> about
             } else {
                 this.nextElementSibling.setAttribute("style", "display:block");
                 this.nextElementSibling.innerText="* Must be only letters.";
@@ -18,27 +15,18 @@
 
       nombre.onkeypress=soloLetras;
       email.onkeypress=soloLetras;
-
-      /*var correovalido = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-      if (!correovalido.test(email)){
+      
+      var correovalido = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+      if (correovalido.test(email)){
+        this.nextElementSibling.setAttribute("style", "display:none");
+      } else {
+        //return false;
         this.nextElementSibling.setAttribute("style", "display:block");
         this.nextElementSibling.innerText="* Debe colocar un correo válido.";
-      } else {
-        return true;
-      }
-
-      var passvalido = function (elemento) {
-        if (pass.value > 6 && pass.value < 30){
-          return true;
-        } else {
-          this.nextElementSibling.setAttribute("style", "display:block");
-          this.nextElementSibling.innerText="* Su correo debe tener más de 6 caracteres.";
-        }
       }
 
       email.onkeypress=correovalido;
-      pass.onkeypress=passvalido;
-      */
+
 
       var datos = document.getElementsByClassName("form-control");
 
@@ -47,12 +35,9 @@
             this.value="";
             this.nextElementSibling.setAttribute("style", "display:block");
             this.nextElementSibling.innerText="* Obligatory field";
-          } else{
-            this.nextElementSibling.nextElementSibling.innerText="";
-<<<<<<< HEAD
-=======
+          }
+          if (this.nextElementSibling.innerText!=""){
             this.nextElementSibling.setAttribute("style", "display:none");
->>>>>>> about
           }
           var datoCorrecto=this.value.charAt(0).toUpperCase()+this.value.substring(1).toLowerCase();
           this.value=datoCorrecto;
