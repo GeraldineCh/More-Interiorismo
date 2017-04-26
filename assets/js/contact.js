017fdf81a1afb8d1022961e3bfb0bc708f26986e
nakarid <hamdal@gmail.com>
2017-04-26 11:11:16 -0500

    contacto nuevamente

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,22 +1,22 @@
+var nombre = document.getElementById("name");
+var email = document.getElementById("input-email");
 
-      var nombre = document.getElementById("name");
-      var email = document.getElementById("input-email");
-
-      var soloLetras=function(elemento){
+var soloLetras=function(elemento){
             var letraAscii = elemento.keyCode;
-            if((letraAscii >= 97 && letraAscii <= 122) || (letraAscii >= 65 && letraAscii <= 90) || (letraAscii <= 164 && letraAscii <=165) || letraAscii == 32){
+            if((letraAscii >= 97 && letraAscii <= 122) || (letraAscii >= 65 && letraAscii <= 90) || (letraAscii >= 164 && letraAscii <=165) || letraAscii == 32){
                 return true;
                 this.nextElementSibling.setAttribute("style", "display:none");
             } else {
+              return false;
                 this.nextElementSibling.setAttribute("style", "display:block");
                 this.nextElementSibling.innerText="* Must be only letters.";
             }
-      }
+}
 
-      nombre.onkeypress=soloLetras;
-      email.onkeypress=soloLetras;
-      
-      var correovalido = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+nombre.onkeypress=soloLetras;
+email.onkeypress=soloLetras;
+
+/*   var correovalido = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
       if (correovalido.test(email)){
         this.nextElementSibling.setAttribute("style", "display:none");
       } else {
@@ -27,24 +27,35 @@
 
       email.onkeypress=correovalido;
 
+*/
+var datos = document.getElementsByClassName("form-control");
 
-      var datos = document.getElementsByClassName("form-control");
-
-      var validateForm = function(elemento){
-          if (this.value.trim().length==0){
-            this.value="";
-            this.nextElementSibling.setAttribute("style", "display:block");
-            this.nextElementSibling.innerText="* Obligatory field";
-          }
-          if (this.nextElementSibling.innerText!=""){
-            this.nextElementSibling.setAttribute("style", "display:none");
-          }
-          var datoCorrecto=this.value.charAt(0).toUpperCase()+this.value.substring(1).toLowerCase();
-          this.value=datoCorrecto;
+var validateForm = function(elemento){
+      if (this.value.trim().length==0){
+          this.value="";
+          this.nextElementSibling.setAttribute("style", "display:block");
+          this.nextElementSibling.innerText="* Obligatory field";
       }
+      if (this.value.trim().length!=0){
+          this.nextElementSibling.setAttribute("style", "display:none");
+      }
+      var datoCorrecto=this.value.charAt(0).toUpperCase()+this.value.substring(1).toLowerCase();
+      this.value=datoCorrecto;
+}
 
-      for (var i=0; i<datos.length; i++){
+for (var i=0; i<datos.length; i++){
       datos[i].onblur=validateForm;
-      }
+}
 
-      var boton = document.getElementById("login");
+var boton = document.getElementById("login");
+
+boton.addEventListener("click", function () {
+  var nombre = document.getElementById("name").value;
+  var email = document.getElementById("input-email").value;
+  var datos=[];
+  var usuario= new cliente(nombre,email);
+  console.console.log(new cliente(nombre, email));
+  localStorage.setItem("nuevoUsuario",JSON.stringify(new cliente(nombre,email)));
+  document.getElementById("formulario").reset();
+  //window.location.assign("");
+});
