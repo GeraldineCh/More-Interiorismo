2288e6a104a516efb8da6c46c8d571cd2df7a1ee
Mishmore <michellemore28@gmail.com>
2017-04-26 11:03:14 -0500

    contact line

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -5,14 +5,14 @@ window.addEventListener("scroll", function(){
      document.getElementsByClassName("logo")[0].classList.remove("black");
      document.getElementsByClassName("logo")[0].classList.add("white");
      document.getElementsByTagName("h2")[0].classList.remove("top-line");
-     document.getElementsByTagName("h2")[0].classList.add("top-line-hidden");
+     document.getElementsByTagName("h2")[0].classList.add("top-line-hidden-black");
    }
    if (currentScroll > 300) {
      document.getElementById("about").classList.remove("opacity");
    }
    if (currentScroll > 500) {
      document.getElementsByTagName("h2")[0].classList.add("top-line");
-     document.getElementsByTagName("h2")[0].classList.remove("top-line-hidden");
+     document.getElementsByTagName("h2")[0].classList.remove("top-line-hidden-black");
    }
    if ((currentScroll < 1000) && (currentScroll > 300)){
      document.getElementsByClassName("logo")[0].classList.remove("white");
@@ -26,10 +26,16 @@ window.addEventListener("scroll", function(){
    if (currentScroll > 1000){
      document.getElementsByClassName("logo")[0].classList.remove("black");
      document.getElementsByClassName("logo")[0].classList.add("white");
+     document.getElementsByTagName("h2")[0].classList.remove("top-line");
+     document.getElementsByTagName("h2")[0].classList.add("top-line-hidden-black");
      document.getElementsByTagName("h2")[1].classList.add("top-line");
      document.getElementsByTagName("h2")[1].classList.remove("top-line-hidden");
-     document.getElementsByTagName("h2")[0].classList.remove("top-line");
-     document.getElementsByTagName("h2")[0].classList.add("top-line-hidden");
+     document.getElementsByTagName("h2")[2].classList.remove("top-line-black");
+     document.getElementsByTagName("h2")[2].classList.add("top-line-hidden-black");
+   }
+   if (currentScroll > 1600){
+     document.getElementsByTagName("h2")[2].classList.add("top-line-black");
+     document.getElementsByTagName("h2")[2].classList.remove("top-line-hidden-black");
    }
    if (currentScroll > 1600) {
      document.getElementById("contact").classList.remove("opacity");
