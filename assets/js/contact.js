334d81799a130790b6e6276b0ac3f6773948b55c
nakarid <hamdal@gmail.com>
2017-04-26 14:11:45 -0500

    final final completo

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,53 +1,49 @@
 var nombre = document.getElementById("name");
 var email = document.getElementById("input-email");
 
-var soloLetras=function(elemento){
-            var letraAscii = elemento.keyCode;
-            if((letraAscii >= 97 && letraAscii <= 122) || (letraAscii >= 65 && letraAscii <= 90) || (letraAscii >= 164 && letraAscii <=165) || letraAscii == 32){
-                return true;
-                this.nextElementSibling.setAttribute("style", "display:none");
-            } else {
-              return false;
-                this.nextElementSibling.setAttribute("style", "display:block");
-                this.nextElementSibling.innerText="* Must be only letters.";
-            }
+//Validando Nombre
+var soloLetras = function(elemento) {
+  var letraAscii = elemento.keyCode;
+  if((letraAscii >= 97 && letraAscii <= 122) || (letraAscii >= 65 && letraAscii <= 90) || (letraAscii >= 164 && letraAscii <=165) || letraAscii == 32){
+      return true;
+      this.nextElementSibling.setAttribute("style", "display:none");
+  } else {
+    return false;
+    this.nextElementSibling.setAttribute("style", "display:block");
+    this.nextElementSibling.innerText="* Must be only letters.";
+  }
 }
+nombre.onkeypress = soloLetras;
 
-nombre.onkeypress=soloLetras;
-//email.onkeypress=soloLetras;
-
+//Validando emal
 var correovalido = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-function validandomail(elemento){
-  if (correovalido.test(email.value)){
+function validandomail(elemento) {
+  if (correovalido.test(email.value)) {
         this.nextElementSibling.setAttribute("style", "display:none");
-      } else {
-        //return false;
-        this.nextElementSibling.setAttribute("style", "display:block");
-        this.nextElementSibling.innerText="* Must write a validate email.";
-      }
+  } else {
+    this.nextElementSibling.setAttribute("style", "display:block");
+    this.nextElementSibling.innerText="* Must write a valid email.";
+  }
 }
+email.onkeyup = validandomail;
+email.onblur = validandomail;
 
-email.onkeyup=validandomail;
-email.onblur=validandomail;
-
-var datos = document.getElementsByClassName("form-control");
-
-var validateForm = function(elemento){
-      if (this.value.trim().length==0){
-          this.value="";
-          this.nextElementSibling.setAttribute("style", "display:block");
-          this.nextElementSibling.innerText="* Obligatory field";
-      }
-      if (this.value.trim().length!=0){
-          this.nextElementSibling.setAttribute("style", "display:none");
-      }
-      var datoCorrecto=this.value.charAt(0).toUpperCase()+this.value.substring(1).toLowerCase();
-      this.value=datoCorrecto;
+//Validando que se ingrese dato
+var validateForm = function(elemento) {
+  if (this.value.trim().length == 0) {
+    this.value = "";
+    this.nextElementSibling.setAttribute("style", "display:block");
+    this.nextElementSibling.innerText="* Obligatory field";
+  }
+  if (this.value.trim().length!=0) {
+    this.nextElementSibling.setAttribute("style", "display:none");
+  }
+  var datoCorrecto = this.value.charAt(0).toUpperCase() + this.value.substring(1).toLowerCase();
+  this.value = datoCorrecto;
 }
+nombre.onblur = validateForm;
 
-nombre.onblur=validateForm;
-
-
-var boton = document.getElementById("login");
-
-document.getElementById("formulario").reset();
+//Limpiar formulario en submit
+document.getElementById("login").addEventListener("click", function() {
+  document.getElementById("formulario").reset();
+})
